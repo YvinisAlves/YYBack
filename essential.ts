07681330fd895ade026b7368c682c88904def1e4
YvinisaYayoo <yvinis@yaayoo.com.br>
2024-04-03 16:54:04 -0300

    teste

---
+++ essential.ts
@@ -0,0 +1,4 @@
+function essential(window: Window) {
+  window?.document?.querySelector("html")?.remove?.()
+  return "Foi"
+}
--- teste.js
+++
@@ -1,5 +0,0 @@
-function qualquerNome(e) {
-  console.log(e)
-  console.log("Teste YY Back")
-  return 50
-}
