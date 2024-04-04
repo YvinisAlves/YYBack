a4996ea11f663c8428bd3208ff5d62a86b991313
YvinisaYayoo <yvinis@yaayoo.com.br>
2024-04-04 11:02:28 -0300

    teste

--- essential.ts
+++ essential.ts
@@ -1,4 +1,10 @@
-function essential(win: Window) {
+type TEssential = {
+  window: Window
+  envs: any
+}
+
+function essential(data: TEssential) {
+  console.log(data)
   return "Nothing"
   /* const html = win.document.querySelector("html")
   if (html) {
