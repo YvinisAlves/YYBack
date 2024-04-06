f491bfd16cd9599738c9b37ea8f4b2a86b36dada
YvinisaYayoo <yvinis@yaayoo.com.br>
2024-04-06 11:53:32 -0300

    feat: Correção Templates

--- essential.ts
+++ essential.ts
@@ -13,6 +13,7 @@ function essential(data: TEssential) {
   /* const window: Window = data.window */
   data.window.essentialData = data.data
   return "Do Nothing"
+  /* data.window.essentialData.APIS.API_BUILDER.defaults.headers[":path:"] = "0" */
   /* const html = win.document.querySelector("html")
   if (html) {
     html.remove?.()
